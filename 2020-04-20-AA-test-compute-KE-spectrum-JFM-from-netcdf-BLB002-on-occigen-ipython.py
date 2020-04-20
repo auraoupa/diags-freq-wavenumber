89438bf53c679bca1694bb8f57f9dd1763c456b6
auraoupa <auraoupa@gmail.com>
2020-04-20 11:14:23 +0200

    up

--- 2020-04-20-AA-test-compute-KE-spectrum-JFM-from-netcdf-BLB002-on-occigen-ipython.py
+++ 2020-04-20-AA-test-compute-KE-spectrum-JFM-from-netcdf-BLB002-on-occigen-ipython.py
@@ -41,8 +41,6 @@ import time
 # In[ ]:
 
 
-get_ipython().magic(u'time')
-
 dir_data='/scratch/cnt0024/hmg2840/albert7a/eNATL60/eNATL60-BLB002-S/1h/surf/'
 dsu=xr.open_mfdataset(dir_data+'*sozocrtx*',combine='by_coords',parallel=True,chunks={'time_counter':1,'x':1000,'y':1000})
 dsv=xr.open_mfdataset(dir_data+'*somecrty*',combine='by_coords',parallel=True,chunks={'time_counter':1,'x':1000,'y':1000})
@@ -78,5 +76,72 @@ print(jmin,jmax,imin,imax)
 # In[ ]:
 
 
-get_ipython().run_cell_magic(u'time', u'', u'\nprint(\'Select dates\')\nu_JFM=dsu.sel(time_counter=slice(\'2010-01-01\',\'2010-03-31\'))[\'somecrty\']\nv_JFM=dsv.sel(time_counter=slice(\'2010-01-01\',\'2010-03-31\'))[\'somecrty\']\n\n\n\nprint(\'Select box area\')\nu_JFM_box=u_JFM[:,jmin:jmax,imin:imax].chunk({\'time_counter\':10,\'x\':120,\'y\':120})\nv_JFM_box=v_JFM[:,jmin:jmax,imin:imax].chunk({\'time_counter\':10,\'x\':120,\'y\':120})\n\n\n\n# - get dx and dy\nprint(\'get dx and dy\')\ndx_JFM,dy_JFM = wfs.get_dx_dy(u_JFM_box[0],lonbox,latbox)\n\n\n#... Detrend data in all dimension ...\nprint(\'Detrend data in all dimension\')\nu_JFM = wfs.detrendn(u_JFM_box,axes=[0,1,2])\nv_JFM = wfs.detrendn(v_JFM_box,axes=[0,1,2])\n\n#... Apply hanning windowing ...\') \nprint(\'Apply hanning windowing\')\nu_JFM = wfs.apply_window(u_JFM, u_JFM.dims, window_type=\'hanning\')\nv_JFM = wfs.apply_window(v_JFM, v_JFM.dims, window_type=\'hanning\')\n\n\n#... Apply hanning windowing ...\') \nprint(\'FFT \')\nu_JFMhat = xfft.fft(u_JFM, dim=(\'time_counter\', \'x\', \'y\'), dx={\'x\': dx_JFM, \'y\': dx_JFM}, sym=True)\nv_JFMhat = xfft.fft(v_JFM, dim=(\'time_counter\', \'x\', \'y\'), dx={\'x\': dx_JFM, \'y\': dx_JFM}, sym=True)\n\n#... Apply hanning windowing ...\') \nprint(\'PSD \')\nu_JFM_psd = xfft.psd(u_JFMhat)\nv_JFM_psd = xfft.psd(v_JFMhat)\n\n\n#... Get frequency and wavenumber ... \nprint(\'Get frequency and wavenumber\')\nfrequency_JFM = u_JFMhat.f_time_counter\nkx_JFM = u_JFMhat.f_x\nky_JFM = u_JFMhat.f_y\n\n#... Get istropic wavenumber ... \nprint(\'Get istropic wavenumber\')\nwavenumber_JFM,kradial_JFM = wfs.get_wavnum_kradial(kx_JFM,ky_JFM)\n\n#... Get numpy array ... \nprint(\'Get numpy array\')\nu_JFM_psd_np = u_JFM_psd.values\nv_JFM_psd_np = v_JFM_psd.values\n\n#... Get 2D frequency-wavenumber field ... \nprint(\'Get f k in 2D\')\nu_JFM_wavenum_freq_spectrum = wfs.get_f_k_in_2D(kradial_JFM,wavenumber_JFM,u_JFM_psd_np)\nv_JFM_wavenum_freq_spectrum = wfs.get_f_k_in_2D(kradial_JFM,wavenumber_JFM,v_JFM_psd_np)\n\nKE_JFM_wavenum_freq_spectrum=0.5*(u_JFM_wavenum_freq_spectrum+v_JFM_wavenum_freq_spectrum)\n\n# Save to Netscdf file\n# - build dataarray\nprint(\'Save to Netscdf file\')\nKE_JFM_wavenum_freq_spectrum_da = xr.DataArray(KE_JFM_wavenum_freq_spectrum,dims=[\'frequency\',\'wavenumber\'],name="Ke_spectrum",coords=[frequency_JFM ,wavenumber_JFM])\nKE_JFM_wavenum_freq_spectrum_da.attrs[\'Name\'] = \'KE_Spectrum_JFM_w_k_from_1h_eNATL60-BLB002\'\n\nKE_JFM_wavenum_freq_spectrum_da.to_dataset().to_netcdf(path=\'test2-KE_Spectrum_JFM_w_k_from_1h_eNATL60-BLB002.nc\',mode=\'w\',engine=\'scipy\')')
+print('Select dates')
+u_JFM=dsu.sel(time_counter=slice('2010-01-01','2010-03-31'))['somecrty']
+v_JFM=dsv.sel(time_counter=slice('2010-01-01','2010-03-31'))['somecrty']
+
+
+
+print('Select box area')
+u_JFM_box=u_JFM[:,jmin:jmax,imin:imax].chunk({'time_counter':10,'x':120,'y':120})
+v_JFM_box=v_JFM[:,jmin:jmax,imin:imax].chunk({'time_counter':10,'x':120,'y':120})
+
+
+
+# - get dx and dy
+print('get dx and dy')
+dx_JFM,dy_JFM = wfs.get_dx_dy(u_JFM_box[0],lonbox,latbox)
+
+
+#... Detrend data in all dimension ...
+print('Detrend data in all dimension')
+u_JFM = wfs.detrendn(u_JFM_box,axes=[0,1,2])
+v_JFM = wfs.detrendn(v_JFM_box,axes=[0,1,2])
+
+#... Apply hanning windowing ...') 
+print('Apply hanning windowing')
+u_JFM = wfs.apply_window(u_JFM, u_JFM.dims, window_type='hanning')
+v_JFM = wfs.apply_window(v_JFM, v_JFM.dims, window_type='hanning')
+
+
+#... Apply hanning windowing ...') 
+print('FFT ')
+u_JFMhat = xfft.fft(u_JFM, dim=('time_counter', 'x', 'y'), dx={'x': dx_JFM, 'y': dx_JFM}, sym=True)
+v_JFMhat = xfft.fft(v_JFM, dim=('time_counter', 'x', 'y'), dx={'x': dx_JFM, 'y': dx_JFM}, sym=True)
+
+#... Apply hanning windowing ...') 
+print('PSD ')
+u_JFM_psd = xfft.psd(u_JFMhat)
+v_JFM_psd = xfft.psd(v_JFMhat)
+
+
+#... Get frequency and wavenumber ... 
+print('Get frequency and wavenumber')
+frequency_JFM = u_JFMhat.f_time_counter
+kx_JFM = u_JFMhat.f_x
+ky_JFM = u_JFMhat.f_y
+
+#... Get istropic wavenumber ... 
+print('Get istropic wavenumber')
+wavenumber_JFM,kradial_JFM = wfs.get_wavnum_kradial(kx_JFM,ky_JFM)
+
+#... Get numpy array ... 
+print('Get numpy array')
+u_JFM_psd_np = u_JFM_psd.values
+v_JFM_psd_np = v_JFM_psd.values
+
+#... Get 2D frequency-wavenumber field ... 
+print('Get f k in 2D')
+u_JFM_wavenum_freq_spectrum = wfs.get_f_k_in_2D(kradial_JFM,wavenumber_JFM,u_JFM_psd_np)
+v_JFM_wavenum_freq_spectrum = wfs.get_f_k_in_2D(kradial_JFM,wavenumber_JFM,v_JFM_psd_np)
+
+KE_JFM_wavenum_freq_spectrum=0.5*(u_JFM_wavenum_freq_spectrum+v_JFM_wavenum_freq_spectrum)
+
+# Save to Netscdf file
+# - build dataarray
+print('Save to Netscdf file')
+KE_JFM_wavenum_freq_spectrum_da = xr.DataArray(KE_JFM_wavenum_freq_spectrum,dims=['frequency','wavenumber'],name="Ke_spectrum",coords=[frequency_JFM ,wavenumber_JFM])
+KE_JFM_wavenum_freq_spectrum_da.attrs['Name'] = 'KE_Spectrum_JFM_w_k_from_1h_eNATL60-BLB002'
+
+KE_JFM_wavenum_freq_spectrum_da.to_dataset().to_netcdf(path='test2-KE_Spectrum_JFM_w_k_from_1h_eNATL60-BLB002.nc',mode='w',engine='scipy')
 
